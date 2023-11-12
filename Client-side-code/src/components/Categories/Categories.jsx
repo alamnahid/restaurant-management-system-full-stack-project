aacdcc65ad04738b276d737c2b4eea4ab217ea26
alamnahid <nahidalam13765@gmail.com>
2023-11-12 20:15:52 +0600

    implement category bar

--- Client-side-code/src/components/Categories/Categories.jsx
+++ Client-side-code/src/components/Categories/Categories.jsx
@@ -1,15 +1,23 @@
+import { useSearchParams } from "react-router-dom";
 import Container from "../Shared/Container";
 import CategorieBox from "./CategorieBox";
 import { categories } from "./categoriesData"
 
 const Categories = () => {
+    const [params, setParams] = useSearchParams()
+    const category = params.get('category')
+    console.log(category)
+
     return (
         <div>
             <Container>
-                <div className="pt-4 flex justify-between items-center overflow-x-auto">
+                <div className="pt-4 mb-8 flex justify-between items-center overflow-x-auto">
                     {
-                        categories?.map(categorie => <CategorieBox
-                            key={categorie.label} icon={categorie.icon} label={categorie.label}
+                        categories?.map(item => <CategorieBox
+                            key={item.label} 
+                            icon={item.icon} 
+                            label={item.label}
+                            selected={category === item.label}
                         ></CategorieBox>)
                     }
                 </div>
