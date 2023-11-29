44bf24f0c762b373639c28e47f015549493aeb18
alamnahid <nahidalam13765@gmail.com>
2023-11-29 20:00:31 +0600

    fix some error

--- Client-side-code/src/main.jsx
+++ Client-side-code/src/main.jsx
@@ -10,14 +10,21 @@ import AuthProvider from './Provider/AuthProvider';
 import 'react-date-range/dist/styles.css'
 import 'react-date-range/dist/theme/default.css'
 import {HelmetProvider } from 'react-helmet-async';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+const queryClient = new QueryClient()
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    <HelmetProvider>
    <AuthProvider>
-    <Toaster />
+   <QueryClientProvider client={queryClient}>
+   <Toaster />
      <RouterProvider router={router} />
+   </QueryClientProvider>
     </AuthProvider>
    </HelmetProvider>
   </React.StrictMode>,
